6a97f304e51b88931618fd83e0ace7d6f27334d8
allejok96 <alexf@openmailbox.org>
2015-12-25 21:32:45 +0100

    Improve readability and comments

--- kodiator.sh
+++ kodiator.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 #
-# kodiator
+# broadcast-index
 #
 # Requires: GNU sed, curl/wget
 #
 # Index tv.jw.org by processing the files from mediator.jw.org
 # and create m3u playlists, which can be played by e.g. Kodi.
-# Thereby the name Kodiator.
 #
-
-cleanup()
-{
-    # The history file needs to be keeped all the time kodiator is running
-    # The CLEANUP variable is so that the subprocesses don't remove it.
-    ((CLEANUP)) && [[ -e /tmp/.kodiator.history ]] && rm "$histfile"
-}
 
 error()
 {
@@ -38,6 +30,14 @@ Usage: kodiator [options] [DIRECTORY]
   
 EOF
     exit
+}
+
+# Remove the history file
+cleanup()
+{
+    # The history file needs to be keeped all the time kodiator is running
+    # The CLEANUP variable is so that the subprocesses don't remove it.
+    ((CLEANUP)) && [[ -e $histfile ]] && rm "$histfile"
 }
 
 # Add newline around squiggly and square brackets and replace commas with newline
@@ -70,27 +70,39 @@ unsquash_file()
 '
 }
 
-# Read the formated json file on stdin
+# Read the formated JSON file on stdin
 # write videos to playlists
 # and download and parse new categories
 #
 # GIGANT CAVEATS:
-# "key" must be followed by "name"
-# "title" must come before "progressiveDownloadURL"
+# - "key" must be followed by "name"
+# - "title" must come before "progressiveDownloadURL"
 #
 parse_lines()
 {
+
+    # Create directory
+    if [[ ! -e $savedir ]]; then
+	mkdir -p "$savedir" || error "$savedir: Failed to create directory"
+    fi
+
+    # Start on an empty playlist
+    if [[ ! -e $savedir/$category.m3u ]]; then
+	    echo "#EXTM3U" > "$savedir/$category.m3u" || error "Failed to write to playlist"
+    fi
+
     # Read stdin
     while read -r; do
 	# Remove quotes from REPLY
 	input="${REPLY//\"/}"
 	case "$input" in
+
 	    key:*)
-		echo $input
 		key="${input#*:}"
 		;;
+
+	    # A new category - parse it
 	    name:*)
-		echo $input
 		name="${input#*:}"
 		# Start a new instance - download and parse category
 		if ((RECURSIVE)) && ! grep -q "$key" "$histfile"; then
@@ -98,60 +110,56 @@ parse_lines()
 		    ("$0" --no-cleanup --category "$key")
 		fi
 		;;
+
+	    # A new clip - save the old one
 	    title:*)
-		echo $input
-		# If there is a title an url, print them
+		# If there is a title an URL, print them
 		if [[ $title && $url ]]; then
 		    print_to_file "# $title" "$url"
-		    # Unset title and url so they don't get repeated
+		    # Unset title and URL so they don't get repeated
 		    unset title url
 		fi
+
 		# Save the new title
 		title="${input#*:}"
 		;;
-	    # If the url is the quality we want - save it
+
+	    # The URL we want - save it
 	    progressiveDownloadURL:*${quality}P.mp4)
 		url="${input#*:}"
 		;;
-	    # If the url is some other quality, check if it's better
+
+	    # Another URL - is it better?
 	    progressiveDownloadURL:*)
 		if [[ $url ]]; then
 		    n="$(sed 's/^.*r\([0-9]\)*P.mp4$/\1/'<<< "${input#*:}")" # new quality
 		    o="$(sed 's/^.*r\([0-9]\)*P.mp4$/\1/' <<< "$url")" # old quality
-		    # Better than the old one, but not bigger than maximum
+		    # Save if it's better than the old one, but not bigger than maximum
 		    [[ $n -gt $o && $n -lt $quality ]] && url="${input#*:}"
 		fi
 		;;
 	esac
     done
 
-    # If there is a title and url left when we reach EOF
+    # If there is a title and an URL left when we reach EOF
     # save them to the playlist
     if [[ $title && $url ]]; then
 	print_to_file "# $title" "$url"
     fi
 }
 
-# Print text to the playlist file that's set
-# in the array $filename
+# Print text to the playlist
 print_to_file()
 {
-    # Create directory
-    if [[ ! -e $savedir ]]; then
-	mkdir -p "$savedir" || error "$savedir: Failed to create directory"
-    fi
-    # Start on an empty file if it doesn't exist or if it isn't in history
-    if [[ ! -e $savedir/$category.m3u ]] || ! grep -q "$category" "$histfile"; then
-	    echo "#EXTM3U" > "$savedir/$category.m3u"
-    fi
     # Write to file
     printf '%s\n' "$@" >> "$savedir/$category.m3u"
-    # Remove the title
+    # Remove the title and URL so they don't get repeated at EOF
     unset title url
 }
 
 # Download the language list and make it readable
-# Note: This is probably not very failsafe
+# CAVEATS:
+# - "name" must be followed by "code"
 lang_list()
 {
     # 1. Download the list
@@ -162,7 +170,7 @@ lang_list()
     # 4. Sort it
     # 5. Switch place on LANG and CODE
     # 6. Make a nice list with columns
-    curl --silent "$1" \
+    download_file "$langurl" \
         | sed 's/{/\n/g' \
         | sed -n 's/.*"name":"\([^"]*\)".*"code":"\([^"]*\)".*/\1:\2/p' \
         | sort \
@@ -170,17 +178,26 @@ lang_list()
         | column -t -s :
 }
 
-# Clean up before exit
-trap 'cleanup' SIGINT SIGTERM SIGHUP EXIT
+# Download a file with curl or wget
+download_file()
+{
+    if ((CURL)); then
+	curl --silent "$1" || error "Failed to download file"
+    else
+	wget --quiet -O - "$1" || error "Failed to download file"
+    fi
+}
 
 # Check requirements
 type sed &>/dev/null || error "This script requires GNU sed"
+
 if type curl &>/dev/null; then
     CURL=1
 else
     type wget &>/dev/null || error "This script requires curl or wget"
     CURL=0
 fi
+
 sed --version | egrep -q "GNU sed" || cat <<EOF
 Warning:
 This script is build for and tested only with GNU sed.
@@ -198,14 +215,11 @@ while [[ $1 ]]; do
         --no-recursive) RECURSIVE=0
             ;;
         --lang)
-	    langurl="http://mediator.jw.org/v1/languages/E/web"
-            if curl --silent "$langurl" | grep -q "\"code\":\"$2\""; then
-                lang="$2"
-                shift
-            else
-                lang_list "$langurl"
-		exit
-            fi
+            if [[ $2 ]]; then
+		lang="$2"
+	    else
+		LANGLIST=1
+	    fi
             ;;
 	--category)
 	    category="$2"
@@ -227,24 +241,40 @@ while [[ $1 ]]; do
     shift
 done
 
+# Assign variables
 export lang savedir quality
-savedir="${1:-./kodiator/$lang}"
+savedir="${1:-./JWBroadcasting-$lang}"
 [[ $CLEANUP ]] || CLEANUP=1
 [[ $lang ]] || lang=E
 [[ $RECURSIVE ]] || RECURSIVE=1
 [[ $category ]] || category=VideoOnDemand
 [[ $quality ]] || quality=480
+histfile="/tmp/.broadcast-index.history"
+baseurl="http://mediator.jw.org/v1/categories/$lang/${category}?detailed=1"
+langurl="http://mediator.jw.org/v1/languages/E/web"
 
+# Check if language is correct
+download_file "$langurl" | grep -q "\"code\":\"$lang\"" || LANGLIST=1
 
-# Check the history if we processed this already
-histfile="/tmp/.kodiator.history"
+# Show the language list, maybe
+if ((LANGLIST)); then
+    lang_list
+    exit
+fi
+
+# Clean up before exit
+trap 'cleanup' SIGINT SIGTERM SIGHUP EXIT
+
+# Write to the history file
+# and show a message with our progress at the same time
+cleanup
 echo "$category" | tee -a "$histfile"
 
-# Download and parse the json file
-baseurl="http://mediator.jw.org/v1/categories/$lang/${category}?detailed=1"
-if ((CURL)); then
-    curl --silent "$baseurl" || error "Failed to download file"
-else
-    wget --quiet -O - "$baseurl" || error "Failed to download file"
-fi | unsquash_file | parse_lines || error
+# Download and parse the JSON file
+download_file "$baseurl" | unsquash_file | parse_lines || error
 
+# Rename the "first" file so it ends up at the top
+# (use CLEANUP to see if we are parent or child process - because we're lazy)
+if ((CLEANUP)); then
+    mv "$storepath/$category.m3u" "$storepath/00 $category.m3u"
+fi
