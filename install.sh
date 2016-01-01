ca72703d6351dc897ab0c80ccf594d23af778e72
allejok96 <allejok96@gmail.com>
2016-01-01 11:13:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ if [[ $1 = --help ]]; then
     exit
 fi
 
+if [[ $UID != 0 ]]; then
+    echo "Must run as root"
+    exit 1
+fi
+
 install -d /usr/local/bin /usr/local/lib/jw-scripts
 install -m 755 ./jwb-index /usr/local/bin/jwb-index
 install -m 755 ./nwt-index /usr/local/bin/nwt-index
-install -m 644 ./shared-functions.sh /usr/local/lib/jw-scripts/shared-functions
+install -m 644 ./shared-functions /usr/local/lib/jw-scripts/shared-functions
