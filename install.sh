8600edc559d161966840a2e8151884fb1c7dec39
Alex <alexf@openmailbox.org>
2016-09-27 21:37:35 +0200

    Include new service file

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
+[[ -e /etc/systemd/system/jwb-rpi.service && ! -e /etc/systemd/system/jwb-rpi-downloader.service ]] \
+    && echo "you may want to run: systemctl enable jwb-rpi-downloader"
+
 install -d /usr/local/bin /usr/local/lib/jw-scripts
 install -m 755 ./jwb-index /usr/local/bin/jwb-index
 install -m 755 ./nwt-index /usr/local/bin/nwt-index
@@ -17,6 +20,7 @@ install -m 755 ./jwb-stream /usr/local/bin/jwb-stream
 install -m 755 ./jwb-rpi /usr/local/bin/jwb-rpi
 install -m 644 ./shared-functions /usr/local/lib/jw-scripts/shared-functions
 install -m 644 ./jwb-rpi.service /etc/systemd/system/jwb-rpi.service
+install -m 644 ./jwb-rpi-downloader.service /etc/systemd/system/jwb-rpi-downloader.service
 
 if [[ -e /etc/jwb-rpi.conf ]]; then
     echo "installing jwb-rpi.conf as /etc/jwb-rpi.conf.new"
