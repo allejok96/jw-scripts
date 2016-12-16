f1b2b8281daf0ebebea964706887959fb22daee3
allejok96 <allejok96@gmail.com>
2016-12-16 19:48:55 +0100

    More systemd

--- install.sh
+++ install.sh
@@ -10,9 +10,6 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
-[[ -e /etc/systemd/system/jwb-rpi.service && ! -e /etc/systemd/system/jwb-rpi-downloader.service ]] \
-    && echo "you may want to run: systemctl enable jwb-rpi-downloader"
-
 install -d /usr/local/bin /usr/local/lib/jw-scripts
 install -m 755 ./jwb-index /usr/local/bin/jwb-index
 install -m 755 ./nwt-index /usr/local/bin/nwt-index
@@ -20,8 +17,8 @@ install -m 755 ./jwb-stream /usr/local/bin/jwb-stream
 install -m 755 ./jwb-rpi /usr/local/bin/jwb-rpi
 install -m 755 ./jwb-rpi-downloader /usr/local/bin/jwb-rpi-downloader
 install -m 644 ./shared-functions /usr/local/lib/jw-scripts/shared-functions
-install -m 644 ./jwb-rpi.service /etc/systemd/system/jwb-rpi.service
-install -m 644 ./jwb-rpi-downloader.service /etc/systemd/system/jwb-rpi-downloader.service
+
+install -m 644 -t /etc/systemd/system/ ./systemd/*
 
 if [[ -e /etc/jwb-rpi.conf ]]; then
     echo "installing jwb-rpi.conf as /etc/jwb-rpi.conf.new"
