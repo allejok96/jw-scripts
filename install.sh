c183dad0d48aa15575061719b2c47c211040a870
Alex <alexf@openmailbox.org>
2017-02-25 17:39:30 +0100

    Add systemd directory + minor changes
    Quotes around variables and longer message about the v1 change.

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ fi
 
 prefix=/usr/local
 
-bin=$prefix/bin
-lib=$prefix/lib/jw-scripts
-share=$prefix/share/jw-scripts
+bin="$prefix/bin"
+lib="$prefix/lib/jw-scripts"
+share="$prefix/share/jw-scripts"
 
-install -d $bin $lib
-install -m 755 -t $bin ./jwb-index ./nwt-index ./jwb-stream ./jwb-rpi ./jwb-rpi-downloader
-install -m 755 -t $lib ./autoimport.sh
-install -m 644 -t $lib ./shared-functions
+install -d "$bin" "$lib" /etc/systemd/system
+install -m 755 -t "$bin" ./jwb-index ./nwt-index ./jwb-stream ./jwb-rpi ./jwb-rpi-downloader
+install -m 755 -t "$lib" ./autoimport.sh
+install -m 644 -t "$lib" ./shared-functions
 install -m 644 -t /etc/systemd/system/ ./systemd/*
 
 if [[ -e /etc/jw.conf ]]; then
@@ -29,4 +29,4 @@ else
     install -m 644 ./jw.conf /etc/jw.conf
 fi
 
-[[ -e /etc/jwb-rpi.conf ]] && echo "Configuration file has been re-done. Please have a look at /etc/jw.conf and then remove /etc/jwb-rpi.conf."
+[[ -e /etc/jwb-rpi.conf ]] && echo "Configuration file and several flags for jw-scripts has been re-done. Please have a look at /etc/jw.conf. Also check any custom systemd units that you have set up. Then remove /etc/jwb-rpi.conf to get rid of this message."
