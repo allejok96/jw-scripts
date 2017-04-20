f9af7457030f1c1ceb8b0f1b5defd731a62ae82a
allejok96 <allejok96@gmail.com>
2017-04-20 21:32:15 +0200

    Arguments, checksums, resuming downloads
    Argument parsing. Download and check files (WIP) This does probably not even run. Making future addition of feature smoother by adding a Media object.

--- jwb-index.py
+++ jwb-index.py
@@ -4,22 +4,76 @@ import urllib.request
 import urllib.parse
 import json
 import os
-#import shutil
+import argparse
+import sys
+import hashlib
 
-def download_media(url, dir=None):
+pj = os.path.join
+
+
+def md5(file):
+    """MD5 a file"""
+
+    hash_md5 = hashlib.md5()
+    with open(file, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def download_media(url, directory=None, size=None, checksum=None, resume=False):
     """Download a file to a directory"""
 
-    if dir is None:
-        dir = option.datadir
+    if directory is None:
+        directory = option.work_dir
 
-    os.makedirs(dir, exist_ok=True)
+    os.makedirs(directory, exist_ok=True)
 
-    u = urllib.parse.urlparse(url)
-    file = os.path.basename(u.path)
-    file = os.path.join(dir, file)
+    base = urllib.parse.urlparse(url).path
+    base = os.path.basename(base)
+    file = pj(directory, base)
 
-    print("Laddar hem " + file)
-    urllib.request.urlretrieve(url, file)
+    for loop in range(0, 2):
+
+        request = None
+
+        if os.path.exists(file):
+
+            # If file is smaller, try to resume download once.
+            fsize = os.path.getsize(file)
+            if size is not None and fsize < size:
+                print('Resuming ' + url, file=sys.stderr)
+                request = urllib.request.Request(url, headers={'Range': 'bytes={0}-' + fsize + '-'})
+            elif fsize == size:
+                if checksum and checksum is not None:
+                    if not checksum == md5(file):
+                        os.remove(file)
+                        print(file + ': deleting, checksum mismatch', file=sys.stderr)
+            else:
+                os.remove(file)
+                print(file + ': deleting, size mismatch', file=sys.stderr)
+
+
+        if request is None:
+            print('Downloading ' + file, file=sys.stderr)
+            request = url
+
+
+        urllib.request.urlretrieve(request, file)
+
+    if os.path.exists(file):
+        if size is not None and not os.path.getsize(file) == size:
+            err = 'size mismatch'
+        elif checksum is not None and not md5(file) == checksum:
+            err = 'checksum mismatch'
+        else:
+            return True
+
+        print(file + ': deleting corrupted file, ' + err, file=sys.stderr)
+        os.remove(file)
+        return False
+
+    return file
 
 
 def truncate_file(file, string='', overwrite=False):
@@ -40,87 +94,101 @@ def truncate_file(file, string='', overwrite=False):
     with open(file, 'w') as f:
         f.write(string)
 
-class Storage:
-    """A place to store stuff"""
+
+def check_file(file, size, checksum):
+    """Validate a file and delete invalid ones"""
+
+
+
+
+class Media:
+    """Object with media info"""
     
     def __init__(self):
-        self.__storage = []
-
-    def add(self, value):
-        if value not in self.__storage:
-            self.__storage.append(value)
-
-    def read(self):
-        return self.__storage
-    
-
-class Output:
-    """Methods to create and handle files"""
-
-    def save_media(self, url, *args, **keywords):
-        if option.download:
-            download_media(url)
-
-    def set_key(self, key, *args, **keywords):
-        pass
-
-    def save_sub(self, *args, **keywords):
-        pass
+        self.url = None
+        self.name = None
+        self.checksum = None
+        self.time = None
+        self.size = None
+        self.file = None
 
 
-class OutputM3U(Output):
+class OutputStdout:
+    """Simply output to stdout"""
+
+    def __init__(self):
+        self.media_dir = option.work_dir
+
+    def save_media(self, media):
+        if media.file is not None:
+            print(os.path.relpath(media.file, option.work_dir))
+        else:
+            print(media.url)
+
+    def save_subcat(self, cat, name):
+        return
+
+    def set_cat(self, cat, name):
+        return
+
+
+class OutputM3U(OutputStdout):
     """Create a M3U playlist tree"""
 
     def __init__(self):
-        self.output_file = None
-        self.file_ending = ".m3u"
-        
-    def save_media(self, url, name, *args, **keywords):
+        super().__init__()
+        self._output_file = None
+        self.file_ending = '.m3u'
+        self.media_dir = pj(option.work_dir, option.subdir)
+
+    def save_media(self, media):
         # Write media to a playlist
 
-        if option.download == True:
-            dir = os.path.join(option.datadir, option.subdir)
-            download_media(url, dir=dir)
+        if media.file is not None:
+            source = pj('.', option.subdir, os.path.basename(media.file))
+        else:
+            source = media.url
 
-        self.write_to_file(url, name)
+        self.write_to_file(source, media.name)
 
-    def save_sub(self, key, name, *args, **keywords):
+    def save_subcat(self, cat, name):
         # Write a link to another playlist to the current playlist
 
         name = name.upper()
 
-        dest = os.path.join (".", self._inserted_subdir, key + self.file_ending)
-        self.write_to_file(dest, name)
+        source = pj('.', self._inserted_subdir, cat + self.file_ending)
+        self.write_to_file(source, name)
 
-    def set_key(self, key, *args, **keywords):
+    def set_cat(self, cat, name):
         # Switch to a new file
 
-        if self.output_file is None:
+        if self._output_file is None:
             # The first time this method runs:
             # The current (first) file gets saved outside the subdir,
             # all other data (later files) gets saved inside the subdir,
             # so all paths in the current file must have the subdir prepended.
             self._inserted_subdir = option.subdir
-            self.output_file = os.path.join(option.datadir, key + self.file_ending)
+            self._output_file = pj(option.work_dir, cat + self.file_ending)
         else:
             # The second time and forth:
             # Don't prepend the subdir no more
             # Save data directly in the subdir
             self._inserted_subdir = ''
-            self.output_file = os.path.join(option.datadir, option.subdir, key + self.file_ending)
+            self._output_file = pj(option.work_dir, option.subdir, cat + self.file_ending)
 
         # Since we want to start on a clean file, remove the old one
-        if os.path.exists(self.output_file):
-            os.remove(self.output_file)
+        if os.path.exists(self._output_file):
+            os.remove(self._output_file)
 
-    def write_to_m3u(self, dest, name, file=None):
+    def write_to_m3u(self, source, name, file=None):
         # Write something to a M3U file
 
-        if file == None: file = self.output_file
-        
+        if file is None:
+            file = self._output_file
+
         truncate_file(file, string='#EXTM3U\n')
         with open(file, 'a') as f:
-            f.write('#EXTINF:0,' + name + '\n' + dest + '\n')
+            f.write('#EXTINF:0,' + name + '\n' + source + '\n')
 
     write_to_file = write_to_m3u
 
@@ -130,19 +198,19 @@ class OutputM3UCompat(OutputM3U):
 
     def __init__(self):
         super().__init__()
-        self.file_ending = ".m3u"
+        self.file_ending = '.m3u'
+        self._output_file = None
 
-    def set_key(self, key, name, *args, **keywords):
+    def set_cat(self, cat, name):
         # Switch to a new file
 
-        filename = key + " - " + name + self.file_ending
-        self.output_file = os.path.join(option.datadir, filename)
+        self._output_file = pj(option.work_dir, cat + ' - ' + name + self.file_ending)
 
         # Since we want to start on a clean file, remove the old one
-        if os.path.exists(self.output_file):
-            os.remove(self.output_file)
+        if os.path.exists(self._output_file):
+            os.remove(self._output_file)
 
-    def save_sub(self, *args, **keywords):
+    def save_subcat(self, cat, name):
         # Don't link to other playlists to not crash media players
         # hence the "compat" part of M3UCompat 
         pass
@@ -153,160 +221,207 @@ class OutputHTML(OutputM3U):
 
     def __init__(self):
         super().__init__()
-        self.file_ending = ".html"
+        self.file_ending = '.html'
+        self._output_file = None
 
-    def write_to_html(self, dest, name, file=None):
+    def write_to_html(self, source, name, file=None):
         # Write a link to a HTML file
 
         if file is None:
-            file = self.output_file
+            file = self._output_file
 
         truncate_file(file, string='<!DOCTYPE html>\n<head><meta charset="utf-8" /></head>\n')
 
         with open(file, 'a') as f:
-            f.write('<a href="{0}">{1}</a><br>'.format(dest, name))
+            f.write('<a href="{0}">{1}</a><br>'.format(source, name))
 
     write_to_file = write_to_html
 
 
-class OutputStdout(Output):
-    """Simply output to stdout"""
-    
-    def save_media(self, url, *args, **keywords):
-        if option.download:
-            download_media(url, dir=option.datadir)
-
-        print(url)
-
-
-class OutputFilesystem(Output):
+class OutputFilesystem(OutputStdout):
     """Create a directory structure"""
 
     def __init__(self):
-        self.output_dir = None
+        super().__init__()
+        self._output_dir = None
+        self.media_dir = pj(option.work_dir, option.subdir)
 
-    def set_key(self, key, name, *args, **keywords):
+    def set_cat(self, cat, name):
+        # Create a directory for saving stuff
 
-        self.output_dir = os.path.join(option.datadir, option.subdir, key)
+        first = self._output_dir is None
 
-        # First time: create a symlink outside the subdir
-        if self.output_dir is None:
-            l = os.path.join(option.datadir, name)
-            os.symlink(self.output_dir, l)
+        self._output_dir = pj(option.work_dir, option.subdir, cat)
 
-    def save_sub(self, key, name, *args, **keywords):
-        """Create a symlink to a directory"""
+        if not os.path.exists(self._output_dir):
+            os.makedirs(self._output_dir)
 
-        d = os.path.join(option.datadir, key)
-        l = os.path.join(self.output_dir, name)
+        # First time: create link outside subdir
+        if first:
+            link = pj(option.work_dir, name)
+            if not os.path.lexists(link):
+                dir_fd = os.open(self._output_dir, os.O_RDONLY)
+                # Note: the source will be relative
+                source = pj(option.subdir, cat)
+                os.symlink(source, link, dir_fd=dir_fd)
 
-        if not os.path.exists(d):
-            os.makedirs(d)
+    def save_subcat(self, cat, name):
+        # Create a symlink to a directory
 
-        os.symlink(d, l)
+        dir_ = pj(option.work_dir, option.subdir, cat)
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
 
-    def save_media(self, url, name, *args, **keywords):
-        """Create a symlink to some media"""
+        source = pj('..', cat)
+        link = pj(self._output_dir, name)
 
-        if option.download == True:
-            dir = os.path.join(option.datadir, option.subdir)
-            download_media(url, dir=dir)
+        if not os.path.lexists(link):
+            dir_fd = os.open(self._output_dir, os.O_RDONLY)
+            os.symlink(source, link, dir_fd=dir_fd)
 
-        f = urllib.parse.urlparse(url)
-        f = os.basename(f.path)
-        f = os.path.join(option.datadir, f)
-        ext = os.path.splitext(f)["ext"]
-        l = os.path.join(self.output_dir, name + ext)
-        os.symlink(f, l)
+    def save_media(self, media):
+        # Create a symlink to some media
+
+        if media.file is None:
+            return
+
+        source = pj('..', os.path.basename(media.file))
+        ext = os.path.splitext(media.file)[1]
+        link = pj(self._output_dir, media.name + ext)
+
+        if not os.path.lexists(link):
+            dir_fd = os.open(self._output_dir, os.O_RDONLY)
+            os.symlink(source, link, dir_fd=dir_fd)
 
 
-class Options:
-    """Put global options in here"""
-
-    def __init__(self):
-        self.lang = "E"
-        self.subtitle = False
-        self.quality = 720
-        self.key = "VideoOnDemand"
-        self.mode = "stdout"
-        self.download = False
-        self.subdir = "jwb-" + self.lang
-
-    @property
-    def lang(self):
-        return self.__lang
-
-    @lang.setter
-    def lang(self, code):
-        self.__lang = code
-        self.subdir = "jwb-" + code
-
-        
-def parse_vod(key):
+def parse_vod(cat):
     """Download JSON and do stuff"""
 
-    history.add(key)
+    history.add(cat)
 
-    url = "https://mediator.jw.org/v1/categories/{0}/{1}?detailed=1".format(option.lang, key)
+    url = 'https://mediator.jw.org/v1/categories/{0}/{1}?detailed=1'.format(option.lang, cat)
     with urllib.request.urlopen(url) as response:
         response = json.load(response)
 
-        if "status" in response and response["status"] == "404":
-            print("No such category")
-            exit()
+        if 'status' in response and response['status'] == '404':
+            print('No such category', file=sys.stderr)
+            quit()
 
-        output.set_key(response["category"]["key"], response["category"]["name"])
+        # Set output destination based on current category
+        cat = response['category']['key']
+        name = response['category']['name']
+        print('{0} - {1}'.format(cat, name), file=sys.stderr)
+        output.set_cat(cat, name)
 
-        if "subcategories" in response["category"]:
-            for subcategory in response["category"]["subcategories"]:
-                subkey = subcategory["key"]
-                if subkey not in history.read():
-                    output.save_sub(subkey, subcategory["name"])
-                    queue.add(subkey)
+        # Add subcategories to the queue
+        if 'subcategories' in response['category']:
+            for s in response['category']['subcategories']:
+                if s['key'] not in history:
+                    output.save_subcat(s['key'], s['name'])
+                    queue.add(s['key'])
 
-        if "media" in response["category"]:
-            for media in response["category"]["media"]:
-                video = get_best_video(media["files"])
-                output.save_media(
-                    url=video["progressiveDownloadURL"],
-                    name=media["title"],
-                    size=video["filesize"],
-                    time=video["modifiedDatetime"],
-                    checksum=video["checksum"])
+        # Output media data to current destination
+        if 'media' in response['category']:
+            for media in response['category']['media']:
+                video = get_best_video(media['files'])
 
+                m = Media()
+                m.url = video['progressiveDownloadURL']
+                m.name = media['title']
+
+                if 'firstPublished' in media:
+                    m.date = media['firstPublished']
+                if 'checksum' in video:
+                    m.checksum = video['checksum']
+                if 'size' in video:
+                    m.size = video['size']
+
+                if option.download:
+                    file = download_media(m.url, output.media_dir, resume=True)
+                    if os.path.exists(file):
+                        m.file = file
+
+                output.save_media(m)
                 
+
 def get_best_video(files):
     videos = sorted([x for x in files if x['frameHeight'] <= option.quality],
                     reverse=True,
                     key=lambda v: v['frameWidth'])
-    videos = sorted(videos, reverse=True, key=lambda v: v['subtitled'] == option.subtitle)
+    videos = sorted(videos, reverse=True, key=lambda v: v['subtitled'] == option.subtitles)
     return videos[0]
 
 
-option = Options()
-option.datadir = "/home/alex/mapp"
-option.subtitle = True
-option.quality = 240
-option.key = "VODChildren"
-option.mode = "filesystem"
-option.download = False
-option.lang = "Z"
+# flags, action, nargs, const, default, type, choices, required, help, metavar, dest
+parser = argparse.ArgumentParser(prog='jwb-index.py',
+                                 description='Index or download media from tv.jw.org',
+                                 usage='%(prog)s [options] [DIR]')
 
-output = OutputStdout()
-if option.mode == "stdout":
-    output = OutputStdout()
-elif option.mode == "m3ucompat":
-    output = OutputM3UCompat()
-elif option.mode == "m3u":
-    output = OutputM3U()
-elif option.mode == "filesystem":
-    output = OutputFilesystem()
-elif option.mode == "html":
-    output = OutputHTML()
+parser.add_argument('--quality',
+                    default=720,
+                    type=int,
+                    choices=[240, 360, 480, 720],
+                    help='maximum video quality')
 
-history = Storage()
-queue = Storage()
-queue.add(option.key)
+parser.add_argument('--mode',
+                    default='stdout',
+                    choices=['stdout', 'filesystem', 'm3u', 'm3ucompat', 'html'],
+                    help='output mode')
 
-for key in queue.read():
-    parse_vod(key)
+parser.add_argument('--subtitles',
+                    action='store_true',
+                    help='prefer subtitled videos')
+
+parser.add_argument('--category',
+                    default='VideoOnDemand',
+                    help='category/section to index',
+                    dest='category')
+
+parser.add_argument('--lang',
+                    default='E',
+                    help='language code')
+
+parser.add_argument('--download',
+                    action='store_true',
+                    help='download media')
+
+parser.add_argument('work_dir',
+                    action='store',
+                    nargs='?',
+                    default=os.getcwd(),
+                    help='directory to save data in',
+                    metavar='DIR')
+
+
+
+# HACKISH
+argv = ['--lang', 'Z',
+        '--mode', 'stdout',
+        '--download',
+        '--category', 'ChildrenFeatured',
+        '/home/alex/mapp',
+        '--quality', '240',
+        '--download']
+
+
+
+option = parser.parse_args(argv)
+option.subdir = pj(option.work_dir, 'jwb-' + option.lang)
+
+print(option)
+
+modes = {'stdout': OutputStdout,
+         'm3u': OutputM3U,
+         'm3ucompat': OutputM3UCompat,
+         'filesystem': OutputFilesystem,
+         'html': OutputHTML}
+
+output = modes[option.mode]()
+
+# Begin with the first category
+# (more categories will be added as we move on)
+queue = {option.category}
+history = set()
+
+for c in queue:
+    parse_vod(c)
