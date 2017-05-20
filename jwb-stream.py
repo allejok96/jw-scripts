15db51fd5bcfb1f980608386e1a44ff43f276667
allejok96 <allejok96@gmail.com>
2017-05-20 21:22:19 +0200

    Renaming of lib, mandatory argument.

--- jwb-stream.py
+++ jwb-stream.py
@@ -1,44 +1,56 @@
 #!/usr/bin/env python3
-import argparse
-import jwindex
 import time
 import subprocess
 
+import argparse
 
-parser = argparse.ArgumentParser(prog='jwb-stream.py', usage='%(prog)s [options]',
+import jwlib
+
+
+parser = argparse.ArgumentParser(prog='jwb-stream.py',
+                                 usage='%(prog)s [options]',
                                  description='Stream media from tv.jw.org')
 # TODO
 parser.add_argument('--config')
-
-parser.add_argument('--lang', nargs='?', default='E',
+parser.add_argument('--lang',
+                    nargs='?',
+                    default='E',
                     help='language code')
-
-parser.add_argument('--channel', default='OurStudio', dest='channel',
+parser.add_argument('--channel',
+                    default='OurStudio',
+                    dest='channel',
                     choices=['OurStudio', 'Children', 'Teenagers', 'Family', 'ProgramEvents', 'OurActivities',
                              'Ministry', 'OurOrganization', 'Bible', 'Movies', 'MusicVideo', 'IntExp'],
                     help='channel to stream')
-
-parser.add_argument('--quality', default=720, type=int, choices=[240, 360, 480, 720],
+parser.add_argument('--quality',
+                    default=720,
+                    type=int,
+                    choices=[240, 360, 480, 720],
                     help='maximum video quality')
-
-parser.add_argument('--subtitles', action='store_true')
-
-parser.add_argument('--no-subtitles', action='store_false', dest='subtitles',
+parser.add_argument('--subtitles',
+                    action='store_true')
+parser.add_argument('--no-subtitles',
+                    action='store_false',
+                    dest='subtitles',
                     help='prefer un-subtitled videos')
+parser.add_argument('cmd',
+                    nargs=argparse.REMAINDER)
 
-parser.add_argument('cmd', nargs=argparse.REMAINDER)
 
-
-jwb = jwindex.JWBroadcasting()
-output = jwindex.OutputStreaming()
+jwb = jwlib.JWBroadcasting()
+output = jwlib.OutputStreaming()
 
 jwb.streaming = True
+jwb.quiet = 1
 jwb.utc_offset = (-time.altzone // 60)
 
 parser.parse_args(namespace=jwb)
-jwb.category = 'Streaming' + jwb.channel # set by argparse
+jwb.category = 'Streaming' + jwb.channel  # set by argparse
 
-# Replace {} with 0 in command
+if not jwb.cmd:
+    raise RuntimeError("Not enough arguments")
+
+# Replace the occurrences of `{}` with `0` in the command
 cmd = [arg.replace('{}', '0') for arg in jwb.cmd]
 
 first = True
