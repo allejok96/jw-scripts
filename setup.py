b9d69b0541565af711f4e0d416f9ff8e0bb419ab
allejok96 <allejok96@gmail.com>
2017-06-04 12:25:35 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jw-scripts',
-    version='1.0a1',
+    version='1.0a2',
     description='Download media from jw.org',
     url='https://github.com/allejok96/jw-scripts',
     license='GPL',
