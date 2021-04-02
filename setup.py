9d46cde1f88b18c738678eba1068869df74548f7
allejok96 <allejok96@gmail.com>
2021-04-02 10:50:11 +0200

    v1.10
    Have I really included urllib3 as a dependency since like forever
    without even using it? Man, I suck at this.

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup
 
 setup(
     name='jw-scripts',
-    version='1.0',
+    version='1.10',
     description='Download media from jw.org',
     url='https://github.com/allejok96/jw-scripts',
     license='GPL',
     packages=['jwlib'],
-    install_requires=['urllib3'],
-    #scripts=['jwb-index', 'jwb-offline']
     entry_points={'console_scripts': [
         'jwb-index=jwlib.main:main',
         'jwb-offline=jwlib.player:main'
